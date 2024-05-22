7ff92bf39167f68c67ae071068964304d3c451ea
yisusmartinez5 <gael.jesus.martinez55@gmail.com>
2024-05-22 11:47:23 -0600

    Update contraseña_segura.py

--- contraseña_segura.py
+++ contraseña_segura.py
@@ -1,7 +1,7 @@
 while True:
     contraseña = input("introduce una contraseña segura (minima de 8 caracteres) ")
     if len(contraseña) >= 8:
-        print("contraseña segura tregistrada ")
+        print("contraseña segura registrada ")
         break
     else:
         print("error: la contraseña debe tener minimo 8 caracteres")
