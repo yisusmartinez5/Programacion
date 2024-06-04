1cddc461b23444528984850b63e13ac393d2bd6c
yisusmartinez5 <gael.jesus.martinez55@gmail.com>
2024-06-04 10:32:17 -0600

    Create uso de enumarate.py

---
+++ uso de enumarate.py
@@ -0,0 +1,3 @@
+nombres = ["diana", "jesus", "yahir", "ivan", "osiel", "francisco", "luna", "jose", "maria", "fernanda"]
+for indice, nombre in enumerate (nombres):
+    print(f"{indice}: {nombre}")
