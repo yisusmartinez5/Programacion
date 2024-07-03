329fe2a9fec2cf13dcac787c5b8963723a68ea43
yisusmartinez5 <gael.jesus.martinez55@gmail.com>
2024-07-02 23:56:47 -0600

    Update 2do menu.py

--- 2do menu.py
+++ 2do menu.py
@@ -1,54 +1,54 @@
 #presion
 
-#toneladas
-def convertir_tonelada():
+#libras
+def convertir_libra():
     while True:
-        tonelada_a = input("ingresa la cantidad en toneladas: ")
+        libra_a = input("ingresa la cantidad en libras: ")
         try:
-            if '.' in tonelada_a and tonelada_a.endswith('.'):
+            if '.' in libra_a and libra_a.endswith('.'):
                 print("El dato que has ingresado no tiene decimales. Ingresa de nuevo el dato.")
                 continue
-            tonelada = float(tonelada_a)
-            if tonelada < 0 or tonelada == 0 :
+            libra = float(libra_a)
+            if libra < 0 or libra == 0 :
                 print("El dato que has ingresado esta mal. Ingresa de nuevo el dato. ")
                 continue
-            if '.' in tonelada_a :
-                decimal = tonelada_a.split('.')[1]
+            if '.' in libra_a :
+                decimal = libra_a.split('.')[1]
                 if len(decimal) > 4:
-                    print("El número tiene más de 4 decimales. Ingresa de nuevo la temperatura.")
+                    print("El número tiene más de 4 decimales. Ingresa de nuevo el dato.")
                     continue
-            gramo = tonelada * 1000
-            kilogramo = tonelada * 35.274
-            libra = tonelada * 2.205
-            onza = tonelada /1000
+            gramo = libra * 1000
+            kilogramo = libra * 35.274
+            tonelada = libra * 2.205
+            onza = libra /1000
             
             #eliminar ceros que no nos sirvan al momento de imprimir 
-            resultado_ton = ('{:.4f}'.format(tonelada)).rstrip('0').rstrip('.')
+            resultado_lib = ('{:.4f}'.format(libra)).rstrip('0').rstrip('.')
             resultado_gra = ('{:.4f}'.format(gramo)).rstrip('0').rstrip('.')
             resultado_kil = ('{:.4f}'.format(kilogramo)).rstrip('0').rstrip('.')
-            resultado_lib = ('{:.4f}'.format(libra)).rstrip('0').rstrip('.')
+            resultado_ton = ('{:.4f}'.format(tonelada)).rstrip('0').rstrip('.')
             resultado_onz = ('{:.4f}'.format(onza)).rstrip('0').rstrip('.')
            
             while True:
-             print("\n1. gramos\n2. tonelada\n3. libra\n4. tonelada ")
-             opcion = input("¿Los toneladas ingresados a que unidad la quieres convertir? ")
+             print("\n1. gramos\n2. kilogramos\n3. onzas\n4. toneladas ")
+             opcion = input("¿Los libras ingresados a que unidad la quieres convertir? ")
 
              if opcion == "1":
-                print(f"{resultado_ton} toneladas son {resultado_gra} gramos")
+                print(f"{resultado_ton} libras son {resultado_gra} gramos")
                 break
              elif opcion == "2":
-                print(f"{resultado_ton} toneladas son {resultado_kil} kilogramos")
+                print(f"{resultado_ton} libras son {resultado_kil} kilogramos")
                 break
              elif opcion == "3":
-                print(f"{resultado_ton} toneladas son {resultado_lib} libras")
+                print(f"{resultado_ton} libras son {resultado_lib} libras")
                 break
              elif opcion == "4":
-                 print(f"{resultado_ton} toneladas son {resultado_onz} toneladas")
+                 print(f"{resultado_ton} libras son {resultado_onz} libras")
                  break            
              else:
                 print("Opción no válida. Por favor, elige una opción. ")
             while True:
-             otra_conversion = input("¿Quieres convertir a otra unidad en toneladas? (si/no): ").strip().lower()
+             otra_conversion = input("¿Quieres convertir a otra unidad en libras? (si/no): ").strip().lower()
              if otra_conversion in ['si', 'sí']:
                 break
              elif otra_conversion == 'no':
